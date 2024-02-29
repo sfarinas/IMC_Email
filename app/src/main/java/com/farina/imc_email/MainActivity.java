e64d4bf1fc02ed1ca4c7e94168b29283c653156e
Salomao Farina <sfarinas@gmail.com>
2024-02-28 23:51:02 -0300

    Initial commit

---
+++ app/src/main/java/com/farina/imc_email/MainActivity.java
@@ -0,0 +1,19 @@
+package com.farina.imc_email;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void IMC(View view){
+        
+    }
+}
