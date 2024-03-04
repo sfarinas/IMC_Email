962670433d9a1cc9b97b165e10014754cb858f99
Salomao Farina <sfarinas@gmail.com>
2024-03-04 01:07:04 -0300

    commite update
    Implementado ao envio de email, uma previa de situaçao do peso

--- app/src/main/java/com/farina/imc_email/MainActivity.java
+++ app/src/main/java/com/farina/imc_email/MainActivity.java
@@ -2,6 +2,7 @@ package com.farina.imc_email;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -10,7 +11,12 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    EditText edtNome, edtPeso, edtAltura, edtDestino, edtMensagem;
+    EditText edtNome;
+    EditText edtPeso;
+    EditText edtAltura;
+    EditText edtDestino;
+    EditText edtMensagem;
+    String situacao = "";
     double imc = 0;
 
     @Override
@@ -35,6 +41,30 @@ public class MainActivity extends AppCompatActivity {
             Toast toast = Toast.makeText(getApplicationContext(), "IMC = " +
                     String.format("%.2f", this.imc), Toast.LENGTH_LONG);
             toast.show();
+
+            if (imc < 20.7){
+                this.situacao = "Abaixo do peso.";
+            }
+            else {
+                if (imc < 26.4){
+                    this.situacao = "No peso Normal";
+                }
+                else {
+                    if (imc < 27.4){
+                        this.situacao = "Marginalmente acima do peso";
+                    }
+                    else {
+                        if (imc < 31.1){
+                            this.situacao = "Acima do peso";
+                        }
+                        else {
+                            this.situacao = "Vai Malhar NOW!";
+                        }
+                    }
+                }
+            }
+
+                
         } catch (Exception e){
             Toast toast = Toast.makeText(getApplicationContext(), "Ocorreu algum problema nos dados da Pessoa !",
                     Toast.LENGTH_LONG);
@@ -54,13 +84,14 @@ public class MainActivity extends AppCompatActivity {
         conteudo += "\nPeso: " + edtPeso.getText();
         conteudo += "\nAltura: " + edtAltura.getText();
         conteudo += "\nIMC = " + String.format("%.2f", this.imc);
+        conteudo += "\nSituaçao = " + situacao.toString();
 
         edtMensagem.setText(conteudo);
         emailIntent.putExtra(Intent.EXTRA_TEXT, conteudo);
 
         try {
             startActivity(Intent.createChooser(emailIntent, "Envian E-mail..."));
-        } catch (android.content.ActivityNotFoundException ex){
+        } catch (ActivityNotFoundException ex){
             Toast.makeText(MainActivity.this, "Nao existe Clientes configurados", Toast.LENGTH_LONG).show();
         }
 
